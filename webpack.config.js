44f3eddd40b259b6dd59acbaa7c3bec7e5c4fc81
Vinicius <vinigugelmin@hotmail.com>
2022-05-19 11:55:24 -0300

    feat: add basic structure

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
+
+module.exports = {
+  mode: "development",
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "main.js",
+  },
+  target: "web",
+  devServer: {
+    port: "9500",
+    static: ["./public"],
+    open: true,
+    hot: true,
+    liveReload: true,
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],
+  },
+  plugins: [new Dotenv()],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+};
